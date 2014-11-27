b859debdab5992fc74910d9b8ae5e59fb14421f8
ritvikupadhyaya <ritvikupadhyaya@gmail.com>
2014-11-26 21:17:49 -0600

    Case control in special conditions.

--- key1/KeyboardViewController.m
+++ key1/KeyboardViewController.m
@@ -32,7 +32,8 @@ char ch[63] = {
 
 int currIndex = 0;
 int morseCount = 0;
-
+int mode = 2;
+bool firstLetter = true;
 
 - (void)updateViewConstraints {
     [super updateViewConstraints];
@@ -41,7 +42,9 @@ int morseCount = 0;
 }
 
 - (void)viewDidLoad {
+    mode = 0;
     [super viewDidLoad];
+    [self.textDocumentProxy setAutocapitalizationType:UITextAutocapitalizationTypeSentences];
     self.kbview = [[[NSBundle mainBundle] loadNibNamed:@"kbView" owner:nil options:nil] objectAtIndex:0];
     [self addGesturesToKeyboard];
     self.inputView = self.kbview;
@@ -65,6 +68,8 @@ int morseCount = 0;
 
 - (void)textDidChange:(id<UITextInput>)textInput {
     // The app has just changed the document's contents, the document context has been updated.
+
+    
     
     UIColor *textColor = nil;
     if (self.textDocumentProxy.keyboardAppearance == UIKeyboardAppearanceDark) {
@@ -75,10 +80,10 @@ int morseCount = 0;
     
 }
 
-# pragma mark Keyboards
+# pragma mark Keyboard Event Handler
 
 -(void) refreshChoices {
-    [self.kbview.choiceL setTitle:[NSString stringWithFormat:@"%c", ch[currIndex * 2 + 1]] forState:UIControlStateNormal];
+    [self.kbview.choiceL setTitle:[self stringCase:[NSString stringWithFormat:@"%c", ch[currIndex * 2 + 1]]] forState:UIControlStateNormal];
     [self.kbview.choiceM setTitle:[NSString stringWithFormat:@"%c", ch[currIndex]] forState:UIControlStateNormal];
     [self.kbview.choiceR setTitle:[NSString stringWithFormat:@"%c", ch[currIndex * 2 + 2]] forState:UIControlStateNormal];
 }
@@ -113,8 +118,7 @@ int morseCount = 0;
     [self.kbview.choiceL addTarget:self action:@selector(pressChoiceL) forControlEvents:UIControlEventTouchUpInside];
     [self.kbview.choiceM addTarget:self action:@selector(pressChoiceM) forControlEvents:UIControlEventTouchUpInside];
     [self.kbview.choiceR addTarget:self action:@selector(pressChoiceR) forControlEvents:UIControlEventTouchUpInside];
-    //UISwipeGestureRecognizer *left = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(leftRecog)];
-    //left.direction = UISwipeGestureRecognizerDirectionLeft;
+    
     UILongPressGestureRecognizer *left = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(leftRecog:)];
     left.minimumPressDuration = 1.0;
     left.allowableMovement = 100.0f;
@@ -128,7 +132,18 @@ int morseCount = 0;
     [rightSwipe setDirection:UISwipeGestureRecognizerDirectionRight];
     [self.kbview.dotKey addGestureRecognizer:rightSwipe];
     
+    UISwipeGestureRecognizer *downSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(downSwipe)];
+    [downSwipe setDirection:UISwipeGestureRecognizerDirectionDown];
+    [self.kbview.dotKey addGestureRecognizer:downSwipe];
+    [self.kbview.dashKey addGestureRecognizer:downSwipe];
+    
+    UISwipeGestureRecognizer *upSwipe = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(upSwipe)];
+    [upSwipe setDirection:UISwipeGestureRecognizerDirectionDown];
+    [self.kbview.dashKey addGestureRecognizer:upSwipe];
+    [self.kbview.dotKey addGestureRecognizer:upSwipe];
+
 }
+
 -(void)pressSpaceKey{
     NSString * temp =[self.textDocumentProxy documentContextBeforeInput];
     int len = [temp length];
@@ -145,13 +160,17 @@ int morseCount = 0;
         if([[NSString stringWithFormat:@"%c",[temp characterAtIndex:len] ] isEqual:@" "] &&!([[NSString stringWithFormat:@"%c",[trimmedTemp characterAtIndex:trimmedLen] ] isEqual:@"."])){
                 [self.textDocumentProxy deleteBackward];
             [self.textDocumentProxy insertText:@". "];
+            firstLetter = true;
         }else{
         [self.textDocumentProxy insertText:@" "];
         }
 }
+
 -(void)pressEnterKey{
     [self.textDocumentProxy insertText:@"\n"];
+    firstLetter = true;
 }
+
 -(void)pressDotKey{
     [self.textDocumentProxy insertText:@"."];
     morseCount++;
@@ -162,8 +181,10 @@ int morseCount = 0;
         currIndex = 0;
         morseCount = 0;
     }
+    firstLetter = false;
     [self refreshChoices];
 }
+
 -(void)pressDashKey{
     [self.textDocumentProxy insertText:@"-"];
     morseCount++;   //Outside
@@ -174,8 +195,12 @@ int morseCount = 0;
         currIndex = 0;
         morseCount = 0;
     }
+    firstLetter = false;
     [self refreshChoices];
 }
+
+#pragma mark Swipe Gestures
+
 -(void)leftRecog:(UIPanGestureRecognizer *)gesture{
     [self.textDocumentProxy deleteBackward];
     [self gotoParentNode];
@@ -187,12 +212,56 @@ int morseCount = 0;
 -(void)rightSwipe{
     [self pressChoiceM];    
 }
+-(void)downSwipe{
+    if (mode > 1) {
+        mode--;
+    }
+}
+-(void)upSwipe{
+    if (mode < 3) {
+        mode++;
+    }
+}
+
+#pragma mark Mode Based Insertion Correction
+
+-(NSString *)stringCase:(NSString *)inStr{
+    NSString *outStr = nil;
+    switch (mode) {
+        case 1:
+           outStr = [inStr lowercaseString];
+            return outStr;
+            break;
+        case 2:
+            if (firstLetter) {
+               outStr = [inStr uppercaseString];
+                return outStr;
+            }
+            else{
+                outStr = [inStr lowercaseString];
+                return outStr;
+            }
+            break;
+        case 3:
+            outStr = [inStr uppercaseString];
+            return outStr;
+            break;
+        default:
+            outStr = [inStr lowercaseString];
+            return outStr;
+            break;
+    }
+    return outStr;
+}
+
+
+#pragma mark Choice Selection
 
 -(void) pressChoiceL {
     if (ch[currIndex * 2 + 1] != NULL) {
         int temp = currIndex;
         [self clearTempMorse];
-        [self.textDocumentProxy insertText:[NSString stringWithFormat:@"%c", ch[temp * 2 + 1]]];
+        [self.textDocumentProxy insertText:[self stringCase:[NSString stringWithFormat:@"%c", ch[temp * 2 + 1]]]];
         currIndex = 0;
         morseCount = 0;
         [self refreshChoices];
@@ -202,7 +271,7 @@ int morseCount = 0;
     if (ch[currIndex] != NULL) {
         int temp = currIndex;
         [self clearTempMorse];
-        [self.textDocumentProxy insertText:[NSString stringWithFormat:@"%c", ch[temp]]];
+        [self.textDocumentProxy insertText:[self stringCase:[NSString stringWithFormat:@"%c", ch[temp]]]];
         currIndex = 0;
         morseCount = 0;
         [self refreshChoices];
@@ -212,7 +281,7 @@ int morseCount = 0;
     if (ch[currIndex * 2 + 2] != NULL) {
         int temp = currIndex;
         [self clearTempMorse];
-        [self.textDocumentProxy insertText:[NSString stringWithFormat:@"%c", ch[temp * 2 + 2]]];
+        [self.textDocumentProxy insertText:[ self stringCase:[NSString stringWithFormat:@"%c", ch[temp * 2 + 2]]]];
         currIndex = 0;
         morseCount = 0;
         [self refreshChoices];
